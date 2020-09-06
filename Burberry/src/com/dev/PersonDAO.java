1b8f88a6da57609ecbb63c6dfe331a7cb2a98491
Derek Lorimer <dereklorime@bigpond.com>
2020-09-06 18:23:17 +1000

    DL - MockResultSet

--- Burberry/src/com/dev/PersonDAO.java
+++ Burberry/src/com/dev/PersonDAO.java
@@ -12,6 +12,7 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 import org.hibernate.Session;
+import org.hibernate.jdbc.Work;
 
 import com.database.Person;
 
@@ -19,11 +20,21 @@ public class PersonDAO implements PersonDBLocal {
 
 	private EntityManager em;
 
+	private PersonDAOWork work;
+
+	public void setWork(PersonDAOWork work) {
+		this.work = work;
+	}
+	
+	public static Connection connection;
+
 	public void init() {
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
 
 		em = emf.createEntityManager();
+
+		work = new PersonDAOWork();
 	}
 
 	@Override
@@ -32,32 +43,54 @@ public class PersonDAO implements PersonDBLocal {
 
 		Query query = em.createQuery("SELECT person from Person person");
 		List<Person> people = query.getResultList();
-		
+
 		return people;
 	}
 
 	public void readPerson() throws SQLException {
+
+		Session session = em.unwrap(Session.class);
 		
-		Session hibernateSession = em.unwrap(Session.class);
+		
+	
+		session.doWork(new org.hibernate.jdbc.Work() {
 
-		hibernateSession.doWork(new org.hibernate.jdbc.Work() {
-
-		    @Override
-		    public void execute(Connection connection) throws SQLException {
-		        // do whatever you need to do with the connection
-		    	Statement stmt = connection.createStatement();
+			@Override
+			public void execute(Connection connection) throws SQLException {
+				// do whatever you need to do with the connection
+				PersonDAO.connection = connection;
+				System.out.println("do whatever you need to do with the connection");
+				Statement stmt = connection.createStatement();
 				String sql;
 				sql = "SELECT name From Person";
 				ResultSet rs = stmt.executeQuery(sql);
-				
+
 				while (rs.next()) {
 					System.out.println(rs.getString(1));
 				}
-		    }
+			}
 		});
 
+		session.doWork(work);
+		
+		execute(this.connection);
+		
 		
 	}
+
+	private void execute(Connection connection) throws SQLException {
+		System.out.println("Execute - do whatever you need to do with the connection");
+		Statement stmt = connection.createStatement();
+		String sql;
+		sql = "SELECT name From Person";
+		ResultSet rs = stmt.executeQuery(sql);
+
+		while (rs.next()) {
+			System.out.println(rs.getString("name"));
+		}
+	}
+
+	
 
 	public void setEm(EntityManager em) {
 		this.em = em;
--- Burberry/test/ReadPersonTest.java
+++ Burberry/test/ReadPersonTest.java
@@ -1,42 +1,122 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import org.hibernate.Session;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Matchers;
 import org.mockito.Mockito;
 
 import com.database.Person;
 import com.dev.PersonDAO;
+import com.dev.PersonDAOWork;
 
 public class ReadPersonTest {
 
+	@Ignore
 	@Test
 	public void test() {
-		
+
 		PersonDAO dao = new PersonDAO();
-		
+
 		EntityManager em = Mockito.mock(EntityManager.class);
 		Query query = Mockito.mock(Query.class);
-		
+
 		Mockito.when(em.createQuery("SELECT person from Person person")).thenReturn(query);
-		
+
 		List<Person> people = new ArrayList<Person>();
-		
+
 		Person p = new Person();
 		p.setName("Derek");
 		people.add(p);
-		
+
 		Mockito.when(query.getResultList()).thenReturn(people);
-		
+
 		dao.setEm(em);
-		
+
 		dao.getPerson().forEach(person -> {
 			System.out.println(person.getName());
 		});
+	}
+
+	@Test
+	public void readPerson() throws SQLException {
+
+		PersonDAO dao = new PersonDAO();
+
+		EntityManager em = Mockito.mock(EntityManager.class);
+		Session session = Mockito.mock(Session.class);
+		
+	//	ResultSet rs = Mockito.mock(ResultSet.class);
+		
+		ResultSet rs = MockResultSet.create(
+		           new String[] { "name" }, //columns
+		           new Object[][] { // data
+		             { "Alice" },
+		             { "Bob"},
+		             { "Charles" }
+		           });
+
+		
+		Statement statement = Mockito.mock(Statement.class);
+
+		Query query = Mockito.mock(Query.class);
+		
+		Connection cn = Mockito.mock(Connection.class);
+		Mockito.when(em.unwrap(Connection.class)).thenReturn(cn);
+
+		Mockito.when(em.unwrap(Session.class)).thenReturn(session);
+		
+		Mockito.when(cn.createStatement()).thenReturn(statement);
+		
+		Mockito.when(statement.executeQuery(Matchers.anyString())).thenReturn(rs);
+		
+		dao.connection = cn;
 		
 		
+		PersonDAOWork work = new PersonDAOWork();
+
+	//	doNothing().when(session).doWork(Matchers.anyObject());
+/*
+		doAnswer(new Answer() {
+			public Object answer(InvocationOnMock invocation) {
+				 Object[] args = invocation.getArguments();
+				// Mock mock = (Mock) invocation.getMock();
+			//	 PersonDAO callback = (PersonDAO) invocation.getArguments()[0];
+			    //    session.doWork(work);
+			      
+				
+				System.out.println("Answer...");
+				return null;
+			}
+		}).when(session).doWork(Matchers.anyObject());
+		*/
+		
+		//Mockito.when(session).doWork(Matchers.anyObject()).thenCallRealMethod();
+
+		// Mockito.when(em.createQuery("SELECT person from Person
+		// person")).thenReturn(query);
+
+		// List<Person> people = new ArrayList<Person>();
+
+		// Person p = new Person();
+		// p.setName("Derek");
+		// people.add(p);
+
+		// Mockito.when(query.getResultList()).thenReturn(people);
+
+		dao.setEm(em);
+		dao.setWork(work);
+
+		dao.readPerson();
+
 	}
 
 }
